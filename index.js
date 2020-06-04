b744be03d857688f5a4b875c477500dcf1c67d23
WRHR <wrhreeves@gmail.com>
2020-06-03 21:20:19 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById('main');
+let body = document.querySelector('body');
+
+body.removeChild(main);
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Will is the champion";
+body.appendChild(newHeader);
